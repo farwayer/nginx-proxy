bbcf1842c075596fbb9bf692b990ec2558c98acc
farwayer <farwayer@gmail.com>
2018-10-10 18:03:36 +0300

    less verbose acme

--- acme.sh
+++ acme.sh
@@ -3,5 +3,5 @@
 LICENSE=https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf
 ACCOUNT_KEY=/etc/ssl/acme/account.pem
 
-acme-client -a ${LICENSE} -Nnvv -f ${ACCOUNT_KEY} ${DOMAIN} && renew=1
+acme-client -a ${LICENSE} -Nnv -f ${ACCOUNT_KEY} ${DOMAIN} && renew=1
 [ "$renew" = 1 ] && [ ! $1 ] && nginx -s reload
